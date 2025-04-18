5ae7731c397da83b78fa67c81cc1a553aabeb635
Luis Alberto Perez <luisalbertoperez@gmail.com>
2025-04-18 10:12:23 -0300

    ajustes

--- django_docker/urls.py
+++ django_docker/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from core.urls import urlpatterns as core_urls
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+] + core_urls
