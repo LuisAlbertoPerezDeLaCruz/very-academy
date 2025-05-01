c2fc475509dba73cf5cdfca25400833290e896cc
Luis Alberto Perez <luisalbertoperez@gmail.com>
2025-05-01 08:55:53 -0300

    Corrido nuevamente con lo basico

--- django_docker/settings.py
+++ django_docker/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m*)bl4=awaatx#)=1o042s(59xpd2-jr(hfooq78glalq2%$pw'
+SECRET_KEY = 'django-insecure-b6ickwjumj)*9f6p*p%0v(y4r*&8#))y*cn19n1k$#phjye1d('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
